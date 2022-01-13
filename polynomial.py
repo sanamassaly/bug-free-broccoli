f9d186cc59fdeb1c697a1d3b737a3e0cdb7360b3
Sana Massaly <massalysana@gmail.com>
2022-01-13 20:17:55 +0000

    premier commit du devoir

--- polynomial.py
+++ polynomial.py
@@ -7,15 +7,53 @@ class Poly2:
 
     def __init__(self, *coeffs):
         """ Méthode constructeur qui prend en paramètre, les coefficients du polynôme"""
-        pass
+        self.c = coeffs[0]
+        self.b = coeffs[1]
+        self.a = coeffs[2]
+
 
     def __add__(self, other):
         """Addition 2 polynômes et qui renvoi du nouveau polynôme"""
-        pass
+        self.c = self.c  + other.c
+        self.b = self.b  + other.b
+        self.a = self.a  + other.a
+
+        if self.b< 0:
+            b = f'{self.b}'
+        else:
+            b = f'+{self.b}'
+        
+        if self.c <0:
+            c = f'{self.c}'
+        else:
+            c = f'+{self.c}'
+        
+        
+        message = f"{self.a}X^2 {b}X {c}"
+        return message
 
     def __sub__(self, other):
         """Soustraction de 2 polynômes et renvoi du nouveau polynôme"""
+        self.c = self.c  - other.c
+        self.b = self.b  - other.b
+        self.a = self.a  - other.a
+
+        if self.b< 0:
+            b = f'{self.b}'
+        else:
+            b = f'+{self.b}'
+        
+        if self.c <0:
+            c = f'{self.c}'
+        else:
+            c = f'+{self.c}'
+        
+        
+        message = f"{self.a}X^2 {b}X {c}"
+        return message
         pass
+
+        
 
     def __repr__(self):
         msg = 'Poly2(' + ', '.join([str(c) for c in sorted(self.coeffs.values())]) + ')'
@@ -26,11 +64,32 @@ class Poly2:
         Si: p1 = Poly(3, -4, 2)
         Alors print(p1) affiche: '2X^2 - 4X + 3'
         """
-        pass
+        if self.b< 0:
+            b = f'{self.b}'
+        else:
+            b = f'+{self.b}'
+        
+        if self.c <0:
+            c = f'{self.c}'
+        else:
+            c = f'+{self.c}'
+        
+        message = f"{self.a}X^2 {b}X {c}"
+        return message
 
     def solve(self):
         """ Méthode qui renvoie les solutions si elles existent."""
-        pass
+        self.delta = self.b**2 - 4*(self.a * self.c)
+        if self.delta != 0:
+            self.x1 = (- self.b - sqrt(self.delta))/2*self.a
+            self.x2 = (- self.b + sqrt(self.delta))/2*self.a
+            return self.x1, self.x2
+        else:
+        
+            self.x = -self.b / 2*self.a
+            return self.x
+
+           
 
     def __val(self, x):
         """ Méthode qui calcule et renvoie la valeur de y en fonction de x.
@@ -38,22 +97,33 @@ class Poly2:
         Si: x prend pour valeur 5
         Alors: y = 5^2 + 1 = 26
         """
-        pass
+        self.x = x
+        self.y = self.a*self.x**2 + self.b*self.x + self.c
+        return self.y
+    
 
-    def draw(self, x_points=None):
+    def draw(self, *x_points):
         """ Méthode qui trace la courbe, voir fichier png."""
-        pass
+        self.y = []
+        for elements in x_points:
+            self.y.append(self.__val(elements))
+        plt.plot(self.y,x_points)
+
+
 
 
 if __name__ == "__main__":
     bar = [1, 1, 1]
     p1 = Poly2(*bar)
+    print(p1)
 
     baz = [1, 1, 1]
     p2 = Poly2(*baz)
+    print(p2.solve())
 
     p3 = p1 + p2
     print(p3)  # affiche 2x^2 + 2x + 2
+    
 
     print(p1.solve())  # affiche ((-0.5+0.8660254037844386j), (-0.5-0.8660254037844386j))
     p1.draw()  # trace la courbe de p1, voir fichier png
